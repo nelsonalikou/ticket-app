9f92013eb4841bce9123bac5f6289ab90539dec3
nelsonalikou <nelsonalikou@gmail.com>
2025-07-03 11:54:42 +0200

    add Database, Ticket and User modules

--- ticket-backend/src/app.module.ts
+++ ticket-backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { TicketModule } from './modules/tickets.module';
+import { UserModule } from './modules/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // global access to env variables
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    TicketModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
