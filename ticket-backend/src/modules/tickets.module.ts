178dcce2b59ba590bbe1fbe4691a055c2377abd9
Nelson ALIKOU DONGMO <nelsonalikou@gmail.com>
2025-07-04 04:42:43 +0200

    refactor ticket module to include RabbitMQ integration (for bulk ticket deletion) and listener

--- ticket-backend/src/modules/tickets.module.ts
+++ ticket-backend/src/modules/tickets.module.ts
@@ -4,11 +4,16 @@ import { TicketService } from '../services/ticket.service';
 import { Ticket } from '../database/models/ticket.entity';
 import { User } from '../database/models/user.entity';
 import { TicketController } from '../controllers/ticket.controller.spec';
+import { TicketConsumerListener } from 'src/rabbitmq/rabbitmq.listener';
+import { RabbitMQModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, User])],
+  imports: [
+    TypeOrmModule.forFeature([Ticket, User]),
+    RabbitMQModule,
+  ],
   providers: [TicketService],
-  controllers: [TicketController],
+  controllers: [TicketController, TicketConsumerListener],
   exports: [TicketService],
 })
 export class TicketModule {}
